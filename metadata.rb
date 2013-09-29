5f01290813e7a1065f330e956e7862bb2e30b006
John Leacox <johnlcox@gmail.com>
2013-09-29 11:31:13 -0500

    Add default maven recipe

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name             'maven-crate'
+maintainer       'John Leacox'
+license          'Apache 2.0'
+description      'Installs/Configures maven'
+version          '0.1.0'
+
+%w{
+  ubuntu
+  debian
+  centos
+  redhat
+}.each do |os|
+  supports os
+end
+
+depends 'uncrate_base'
