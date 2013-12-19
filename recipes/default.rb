01b5d31c84d5f2b0b085f228eac6ab7cb7bc5bcf
John Leacox <john.leacox@cerner.com>
2013-12-19 11:11:42 -0600

    Use maven30 brew package for mac

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,27 +3,34 @@ maven_tar_filepath = "#{Chef::Config['file_cache_path']}/#{maven_tar_filename}"
 maven_extract_path = "#{Chef::Config['file_cache_path']}/maven/#{node['maven']['checksum']}"
 maven_install_path = "#{node['maven']['install_path']}/#{node['maven']['install_dir']}"
 
-include_recipe "ark"
+case node[:platform]
+when "centos", "redhat", "debian", "ubuntu"
+  include_recipe "ark"
 
-ark node['maven']['install_dir'] do
-  url node['maven']['download_url']
-  path node['maven']['install_path']
-  checksum node['maven']['checksum']
-  # owner node['uncrate']['user']
-  # group node['uncrate']['user']
-  mode 0755
-  action :put
-end
+  ark node['maven']['install_dir'] do
+    url node['maven']['download_url']
+    path node['maven']['install_path']
+    checksum node['maven']['checksum']
+    # owner node['uncrate']['user']
+    # group node['uncrate']['user']
+    mode 0755
+    action :put
+  end
 
-link "#{node['maven']['install_path']}/#{node['maven']['symlink_dir']}" do
-  to maven_install_path
-  # owner node['uncrate']['user']
-  # group node['uncrate']['user']
-end
+  link "#{node['maven']['install_path']}/#{node['maven']['symlink_dir']}" do
+    to maven_install_path
+    # owner node['uncrate']['user']
+    # group node['uncrate']['user']
+  end
 
-template "#{node['uncrate']['envdir']}/maven.sh" do
-  source "mavenenv.sh.erb"
-  # owner node['uncrate']['user']
-  # group node['uncrate']['user']
-  mode "0755"
+  template "#{node['uncrate']['envdir']}/maven.sh" do
+    source "mavenenv.sh.erb"
+    # owner node['uncrate']['user']
+    # group node['uncrate']['user']
+    mode "0755"
+  end
+when "mac_os_x"
+  package 'maven30' do
+    action :install
+  end
 end
